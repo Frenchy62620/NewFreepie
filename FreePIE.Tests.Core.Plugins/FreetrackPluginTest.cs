9b01e5bc6456cb9e8f0ce390bafaec3de647bdd5
Frenchy62620 <tkiel@free.fr>
2017-10-19 20:25:26 +0200

    Add files via upload

---
+++ FreePIE.Tests.Core.Plugins/FreetrackPluginTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+using System.Threading;
+using FreePIE.Core.Plugins;
+using FreePIE.Tests.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FreePIE.Tests.Core.Plugins
+{
+    [TestClass]
+    public class When_reading_freetrack_data : TestBase
+    {
+        private bool run = true;
+
+        [TestInitialize]
+        public void Context()
+        {
+            var reset = new AutoResetEvent(false);
+            var count = 0;
+            var rand = new Random();
+
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                
+                using (var memoryMappedFile = MemoryMappedFile.CreateOrOpen("FT_SharedMem", Marshal.SizeOf(typeof(FreeTrackData))))
+                {
+                    using (var accessor = memoryMappedFile.CreateViewAccessor())
+                    {
+
+                        while (run)
+                        {
+                            count++;
+                            var model = new FreeTrackData
+                                {
+                                    DataID = count,
+                                    Yaw = rand.Next(0, 5),
+                                    Pitch = rand.Next(5, 10),
+                                    Roll = rand.Next(10, 15),
+                                    X = rand.Next(15, 20),
+                                    Y = rand.Next(20, 25),
+                                    Z = rand.Next(25, 30)
+                                };
+
+                            accessor.Write(0, ref model);
+                     
+                            reset.Set();
+                        }
+                    }
+                }
+            });
+            reset.WaitOne();
+        }
+
+        [TestMethod]
+        public void It_should_read_all_values_from_free_track()
+        {
+
+            var plugin = Get<FreeTrackPlugin>();
+            var global = plugin.CreateGlobal() as FreeTrackGlobal;
+            plugin.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.IsTrue(global.yaw < 5);
+                Assert.IsTrue(global.pitch < 10);
+                Assert.IsTrue(global.roll < 15);
+                Assert.IsTrue(global.x < 20);
+                Assert.IsTrue(global.y < 25);
+                Assert.IsTrue(global.z < 30);
+            }
+
+            plugin.Stop();
+            run = false;
+        }
+    }
+}
---
+++ FreePIE.Tests.Core.Plugins/PPJoyPluginTest.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Windows.Forms;
+using FreePIE.Core.Plugins;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SlimDX.DirectInput;
+
+namespace FreePIE.Tests.Core.Plugins
+{
+    [TestClass]
+    public class When_writing_XY_neutral_values_to_virtual_joystick_device : PPJoyPluginTest
+    {
+        protected override void DoTest(PPJoyGlobal global)
+        {
+        }
+
+        [TestMethod]
+        public void Direct_xy_axis_should_be_centered()
+        {
+            Assert.AreEqual(0, state.X);
+            Assert.AreEqual(0, state.Y);
+        }
+    }
+
+    [TestClass]
+    public class When_writing_XY_max_values_to_virtual_joystick_device : PPJoyPluginTest
+    {
+        protected override void DoTest(PPJoyGlobal global)
+        {
+            global.setAxis(AxisTypes.X, 1000);
+            global.setAxis(AxisTypes.Y, 1000);
+        }
+
+        [TestMethod]
+        public void Direct_xy_axis_should_be_at_max_range()
+        {
+            Assert.AreEqual(1000, state.X);
+            Assert.AreEqual(1000, state.Y);
+        }
+    }
+
+    [TestClass]
+    public class When_writing_XY_min_values_to_virtual_joystick_device : PPJoyPluginTest
+    {
+        protected override void DoTest(PPJoyGlobal global)
+        {
+            global.setAxis(AxisTypes.X, -1000);
+            global.setAxis(AxisTypes.Y, -1000);
+        }
+
+        [TestMethod]
+        public void Direct_xy_axis_should_be_at_min_range()
+        {
+            Assert.AreEqual(-1000, state.X);
+            Assert.AreEqual(-1000, state.Y);
+        }
+    }
+
+    [TestClass]
+    public class When_writing_XY_arbitrary_values_to_virtual_joystick_device : PPJoyPluginTest
+    {
+        protected override void DoTest(PPJoyGlobal global)
+        {
+            global.setAxis(AxisTypes.X, -450);
+            global.setAxis(AxisTypes.Y, 450);
+        }
+
+        [TestMethod]
+        public void Direct_xy_axis_should_be_at_arbitrary_range()
+        {
+            Assert.AreEqual(-450, state.X);
+            Assert.AreEqual(450, state.Y);
+        }
+    }
+
+    public abstract class PPJoyPluginTest
+    {
+        protected JoystickState state;
+        protected abstract void DoTest(PPJoyGlobal global);
+
+        protected PPJoyPluginTest()
+        {
+            var plugin = new PPJoyPlugin();
+            var global = (plugin.CreateGlobal() as PPJoyGlobal[])[0];
+
+            plugin.Start();
+
+            using (var joystick = GetJoystick())
+            {
+                if (joystick == null)
+                    Assert.Fail("No PPJoy virtual stick found");
+
+                DoTest(global);
+                plugin.DoBeforeNextExecute();
+
+                while((joystick.Acquire().IsFailure || joystick.Poll().IsFailure)) { }
+
+                state = joystick.GetCurrentState();
+            }
+
+            plugin.Stop();
+        }
+
+        private Joystick GetJoystick()
+        {
+            var directInput = new DirectInput();
+            var form = new Form();
+
+            foreach (var device in directInput.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly))
+            {
+                var controller = new Joystick(directInput, device.InstanceGuid);
+                controller.SetCooperativeLevel(form.Handle, CooperativeLevel.Exclusive | CooperativeLevel.Background);
+
+                var retries = 0;
+                while (controller.Acquire().IsFailure)
+                {
+                    retries++;
+                    if (retries > 500)
+                        throw new Exception("Couldnt acquire SlimDX stick");
+                }
+
+                if (controller.Information.InstanceName.Contains("PPJoy"))
+                {
+
+                    foreach (DeviceObjectInstance deviceObject in controller.GetObjects())
+                    {
+                        if ((deviceObject.ObjectType & ObjectDeviceType.Axis) != 0)
+                            controller.GetObjectPropertiesById((int)deviceObject.ObjectType).SetRange(-1000, 1000);
+                    }
+
+                    return controller;
+                }
+            }
+
+            return null;
+        }
+    }
+}
