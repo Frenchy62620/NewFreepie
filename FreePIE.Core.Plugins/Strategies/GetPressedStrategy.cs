bfb11cc4f624068e3665f92a696336d40542e469
Thierry Kiel <tkiel@free.fr>
2017-10-22 20:35:04 +0200

    Add files via upload

--- FreePIE.Core.Plugins/Strategies/GetPressedStrategy.cs
+++ FreePIE.Core.Plugins/Strategies/GetPressedStrategy.cs
@@ -6,6 +6,7 @@ using FreePIE.Core.Plugins.Extensions;
 namespace FreePIE.Core.Plugins.Strategies
 {
     using Gx = GlobalExtensionMethods;
+
     public class GetPressedStrategy<T>
     {
         private readonly Func<T, bool, bool> isDown;
@@ -19,10 +20,12 @@ namespace FreePIE.Core.Plugins.Strategies
         {
             public BitArray b;
             public long[] timer;
+            public int h;
             public State()
             {
-                b = new BitArray(2, false); // 0 = pressed/clik, 1 = released/click
-                timer = new long[3]{-1, -1, -1};
+                b = new BitArray(2, false);         // 0 = pressed/clik, 1 = released/click
+                timer = new long[3]{-1, -1, -1};    // 0 = 1clik, 1 = 2clik, 2 = held
+                h = -1;                             // mutliple value Heldown
             }
 
             //public State(int numtimer)
@@ -32,14 +35,14 @@ namespace FreePIE.Core.Plugins.Strategies
             //    timer[numtimer] = Gx.StartCount();
             //}
         }
-         public bool IsPressed(T code, bool value = false)
+        public bool IsPressed(T code, bool value = false)
         {
             State val;
             bool previouslyPressed = dico.TryGetValue(code, out val) && val.b[0];
             if (val == null)
                 dico[code] = (val = new State());
             else
-                val.b[0] = isDown(code, value); 
+                val.b[0] = isDown(code, value);
 
             return previouslyPressed ? false : val.b[0];
         }
@@ -101,12 +104,12 @@ namespace FreePIE.Core.Plugins.Strategies
 
         public bool IsHelDowned(T code, bool value, long duration)
         {
-            if (IsPressed(code))
+            if (IsPressed(code, value))    // || (isDown(code, value) && dico[code].timer[2] < 0))
             {
                 dico[code].timer[2] = Gx.StartCount();
                 return false;
             }
-            if (IsReleased(code))
+            if (IsReleased(code, value))   // || (!isDown(code, value) && dico[code].timer[2] > 0))
             {
                 dico[code].timer[2] = Gx.StopCount();
                 return false;
@@ -117,6 +120,77 @@ namespace FreePIE.Core.Plugins.Strategies
             return false;
         }
 
+        public int IsHelDowned1(T code, bool value, long[] durations)
+        {
+            State val;
+            if (!dico.TryGetValue(code, out val))
+            {
+                dico[code] = (val = new State());
+                return -1;
+            }
+
+            int v = durations.Length;
+            long dur = dico[code].timer[2].GetLapse();
+
+            if (isDown(code, value))
+            {
+                if (dico[code].timer[2] < 0)
+                    dico[code].timer[2] = Gx.StartCount();
+                else
+                {
+                    for (int i = 0; i < v; i++)
+                        if (dur <= durations[i])
+                        {
+                            v = i;
+                            break;
+                        }
+                    if (dico[code].h != v)
+                    {
+                        dico[code].h = v;
+                        return v * 10;
+                    }
+                }
+            }
+            else
+            {
+                if (dico[code].timer[2] >= 0)
+                {
+                    dico[code].timer[2] = Gx.StopCount();
+                    for (int i = 0; i < v; i++)
+                        if (dur <= durations[i])
+                        {
+                            v = i;
+                            break;
+                        }
+                    dico[code].h = -1;
+                    return v;
+                }
+            }
+            return -1;
+        }
+        //public int IsHelDowned(T code, bool value, long [] durations)
+        //{
+        //    if (IsPressed(code, value))    // || (isDown(code, value) && dico[code].timer[2] < 0))
+        //    {
+        //        dico[code].timer[2] = Gx.StartCount();
+        //        return -1;
+        //    }
+        //    if (IsReleased(code, value))   // || (!isDown(code, value) && dico[code].timer[2] > 0))
+        //    {
+        //        dico[code].timer[2] = Gx.StopCount();
+        //        if (value && dico[code].timer[2] >= 0)
+        //            for (int i = 0; i < durations.Length; i++)
+        //                if (dico[code].timer[2].GetLapse() <= durations[i])
+        //                    return i;
+        //            return durations.Length;
+        //    }
+        //    if (value && dico[code].timer[2] >= 0)
+        //        for (int i = durations.Length - 1; i >= 0; i--)
+        //            if (dico[code].timer[2].GetLapse() > durations[i])
+        //                Gx.DecodelineOfCommand("S");
+        //                return i;
+        //    return -1;
+        //}
         public bool Repeated(T code, bool value, long duration)
         {
             if (IsHelDowned(code, value, duration))
