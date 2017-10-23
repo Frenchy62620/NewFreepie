2ba08ea849cabd44818b3807222f9c4acd706b6c
Thierry Kiel <tkiel@free.fr>
2017-10-23 14:06:22 +0200

    Add files via upload

--- FreePIE.Core.Plugins/Strategies/GetPressedStrategy.cs
+++ FreePIE.Core.Plugins/Strategies/GetPressedStrategy.cs
@@ -21,11 +21,11 @@ namespace FreePIE.Core.Plugins.Strategies
             public BitArray b;
             public long[] timer;
             public int h;
-            public State()
+            public State(int l = -1)
             {
                 b = new BitArray(2, false);         // 0 = pressed/clik, 1 = released/click
                 timer = new long[3]{-1, -1, -1};    // 0 = 1clik, 1 = 2clik, 2 = held
-                h = -1;                             // mutliple value Heldown
+                h = l;                             // mutliple value Heldown
             }
 
             //public State(int numtimer)
@@ -102,21 +102,47 @@ namespace FreePIE.Core.Plugins.Strategies
             return false;
         }
 
+        //public bool IsHelDowned1(T code, bool value, long duration)
+        //{
+        //    if (IsPressed(code, value))    // || (isDown(code, value) && dico[code].timer[2] < 0))
+        //    {
+        //        dico[code].timer[2] = Gx.StartCount();
+        //        return false;
+        //    }
+        //    if (IsReleased(code, value))   // || (!isDown(code, value) && dico[code].timer[2] > 0))
+        //    {
+        //        dico[code].timer[2] = Gx.StopCount();
+        //        return false;
+        //    }
+        //    if (value && dico[code].timer[2] >= 0)
+        //        return dico[code].timer[2].GetLapse() >= duration;
+
+        //    return false;
+        //}
         public bool IsHelDowned(T code, bool value, long duration)
         {
-            if (IsPressed(code, value))    // || (isDown(code, value) && dico[code].timer[2] < 0))
+            State val;
+            if (!dico.TryGetValue(code, out val))
             {
-                dico[code].timer[2] = Gx.StartCount();
+                dico[code] = (val = new State());
                 return false;
             }
-            if (IsReleased(code, value))   // || (!isDown(code, value) && dico[code].timer[2] > 0))
-            {
-                dico[code].timer[2] = Gx.StopCount();
-                return false;
-            }
-            if (value && dico[code].timer[2] >= 0)
-                return dico[code].timer[2].GetLapse() >= duration;
 
+            if (value) // (isDown(code, value))
+            {
+                if (dico[code].timer[2] < 0)
+                    dico[code].timer[2] = Gx.StartCount();
+                else
+                {
+                    if (dico[code].timer[2] >= 0)
+                        return dico[code].timer[2].GetLapse() >= duration;
+                }
+            }
+            else
+            {
+                if (dico[code].timer[2] >= 0)
+                    dico[code].timer[2] = Gx.StopCount();
+            }
             return false;
         }
 
@@ -125,72 +151,42 @@ namespace FreePIE.Core.Plugins.Strategies
             State val;
             if (!dico.TryGetValue(code, out val))
             {
-                dico[code] = (val = new State());
-                return -1;
+                dico[code] = val = new State();
             }
-
-            int v = durations.Length;
-            long dur = dico[code].timer[2].GetLapse();
-
-            if (isDown(code, value))
+            int v;
+            if (value) // key down(isDown(code, value))
             {
-                if (dico[code].timer[2] < 0)
-                    dico[code].timer[2] = Gx.StartCount();
+                if (val.timer[2] < 0)
+                    val.timer[2] = Gx.StartCount();
                 else
                 {
-                    for (int i = 0; i < v; i++)
+                    v = durations.Length;
+                    var dur = val.timer[2].GetLapse();
+                    for (var i = 0; i < v; i++)
                         if (dur <= durations[i])
                         {
                             v = i;
                             break;
                         }
-                    if (dico[code].h != v)
+                    if (val.h != v)
                     {
-                        dico[code].h = v;
-                        return v * 10;
+                        val.h = v;
+                        return v + 10;
                     }
                 }
             }
-            else
+            else    // key up
             {
-                if (dico[code].timer[2] >= 0)
-                {
-                    dico[code].timer[2] = Gx.StopCount();
-                    for (int i = 0; i < v; i++)
-                        if (dur <= durations[i])
-                        {
-                            v = i;
-                            break;
-                        }
-                    dico[code].h = -1;
-                    return v;
-                }
+                if (val.timer[2] < 0) return -1;
+
+                val.timer[2] = Gx.StopCount();
+                v = val.h;
+                val.h = -1;
+                return v;
             }
             return -1;
         }
-        //public int IsHelDowned(T code, bool value, long [] durations)
-        //{
-        //    if (IsPressed(code, value))    // || (isDown(code, value) && dico[code].timer[2] < 0))
-        //    {
-        //        dico[code].timer[2] = Gx.StartCount();
-        //        return -1;
-        //    }
-        //    if (IsReleased(code, value))   // || (!isDown(code, value) && dico[code].timer[2] > 0))
-        //    {
-        //        dico[code].timer[2] = Gx.StopCount();
-        //        if (value && dico[code].timer[2] >= 0)
-        //            for (int i = 0; i < durations.Length; i++)
-        //                if (dico[code].timer[2].GetLapse() <= durations[i])
-        //                    return i;
-        //            return durations.Length;
-        //    }
-        //    if (value && dico[code].timer[2] >= 0)
-        //        for (int i = durations.Length - 1; i >= 0; i--)
-        //            if (dico[code].timer[2].GetLapse() > durations[i])
-        //                Gx.DecodelineOfCommand("S");
-        //                return i;
-        //    return -1;
-        //}
+ 
         public bool Repeated(T code, bool value, long duration)
         {
             if (IsHelDowned(code, value, duration))
