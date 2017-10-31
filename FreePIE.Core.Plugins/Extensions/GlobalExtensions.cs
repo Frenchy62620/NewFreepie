fb3fcb54c706444675759e5fa7161e10cb3fa34b
Thierry Kiel <tkiel@free.fr>
2017-10-31 16:40:44 +0100

    Add files via upload

--- FreePIE.Core.Plugins/Extensions/GlobalExtensions.cs
+++ FreePIE.Core.Plugins/Extensions/GlobalExtensions.cs
@@ -397,5 +397,19 @@ namespace FreePIE.Core.Plugins.Extensions
             }
         }
 
+
+        //public static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
+        //{
+        //    if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+        //    try
+        //    {
+        //        return assembly.GetTypes();
+        //    }
+        //    catch (ReflectionTypeLoadException e)
+        //    {
+        //        return e.Types.Where(t => t != null);
+        //    }
+        //}
+
     }
 }
